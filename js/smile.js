8b9bdac9ea8b82785c65d676dd3e9dab778f6e82
shenrm <shenrm@51guanhuai.cn>
2017-01-14 14:56:22 +0800

    update

--- js/smile.js
+++ js/smile.js
@@ -24,14 +24,15 @@
 			var market = 0;			// 当前市值
 			var invest = 0;			// 投入
 			var earnings = 0; 		// 收益
+			var html = null;
 			number = Math.round(money / price);
 			conut = number;
 			market = conut*price;
 			invest = money;
 			earnings = 1;
 			this.log('unsteady:1.0, price:'+price+', number:'+number+', conut:'+conut+', market:'+market+', invest:'+invest+', earnings:'+earnings);
-			this.appendHtml(unsteady, price, number, conut, market, invest, earnings);
-
+			html = this.getHtml(unsteady, price, number, conut, market, invest, earnings);
+			this.appendHtml(html);
 			for (var i = 0; i < length; i++) {
 				if (i > ((length - 2) / 2)) {
 					price = Math.round(price * (1+unsteady));
@@ -44,14 +45,16 @@
 				invest = money*(i+2);
 				earnings = parseFloat(market / invest);
 				this.log('unsteady:'+unsteady+', price:'+price+', number:'+number+', conut:'+conut+', market:'+market+', invest:'+invest+', earnings:'+earnings);
-				this.appendHtml(unsteady, price, number, conut, market, invest, earnings);
+				html = this.getHtml(unsteady, price, number, conut, market, invest, earnings);
+				this.appendHtml(html);
 			}
 		};
-		this.appendHtml(html){
+
+		this.appendHtml = function(html){
 			$('table').append(html);
 		};
 
-		this.getHtml(unsteady, price, number, conut, market, invest, earnings){
+		this.getHtml = function(unsteady, price, number, conut, market, invest, earnings){
 			var html = '<tr><td>'+unsteady+'</td><td>'+price+'</td><td>'+number+'</td><td>'+conut+'</td><td>'+market+'</td><td>'+invest+'</td><td>'+earnings+'</td></tr>';
 			return html;
 		};
