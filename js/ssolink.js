e018a87b8c14d028f254f7816703bf0a74d80adc
shenrm <shenrm@51guanhuai.cn>
2017-09-12 14:24:38 +0800

    添加 微信sso连接

---
+++ js/ssolink.js
@@ -0,0 +1,37 @@
+(function() {
+
+	var $this;
+
+	var ssoHost = 'http://g.aghcdn.com/wxsso?redirect=';
+
+	var Options = function() {
+
+		this.bindEvent = function() {
+			$this = this;
+			$("input[name='encode']").click(function(){
+				var string = $("[name='string']").val();
+				$("[name='result']").val(ssoHost+encodeURIComponent(string));
+			});
+			$("input[name='decode']").click(function(){
+				var string = $("[name='result']").val();
+				$("[name='string']").val(decodeURIComponent(string.split(ssoHost)[1]));
+			});
+		};
+
+		this.init = function() {
+			$this = this;
+			this.bindEvent();
+			return this;
+		};
+
+		this.log = function(info) {
+			console.log(info);
+		}
+	};
+
+	$(function() {
+		var options = new Options();
+		options.init();
+	});
+
+})();
