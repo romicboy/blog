31879b7fea7b9d1f960b640ae08bc7eef5be3094
shenrm <shenrm@51guanhuai.cn>
2017-09-22 15:20:08 +0800

    md5

--- js/md5.js
+++ js/md5.js
@@ -1,13 +1,25 @@
 ﻿(function() {
 
 	var $this;
+
 	var Options = function() {
 
 		this.bindEvent = function() {
 			$this = this;
+
+			$(".dropdown-menu li").click(function() {
+				var value = $(this).find('a').text();
+				$(".dropdown .value").text(value);
+			});
+
 			$("input[name='submit']").click(function(){
+				var type = $(".dropdown .value").text().trim();
 				var str = $("input[name='str']").val();
-				$("input[name='result']").val(md5(str));
+				var result = $("[name='result']");
+				if (type == 'md5') 		result.val(md5(str));
+				if (type == 'sha1') 	result.val(sha1(str));
+				if (type == 'sha256') 	result.val(sha256(str));
+				if (type == 'sha512') 	result.val(sha512(str));
 				return false;
 			});
 		};
